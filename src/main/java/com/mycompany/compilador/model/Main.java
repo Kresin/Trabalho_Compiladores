9b5243c2506e0a22096554b735b1029fe29a178c
Kresin <gabrielkresin@gmail.com>
2022-09-02 17:18:46 -0300

    Criado classe main e criado rotina para dar build do projeto

---
+++ src/main/java/com/mycompany/compilador/model/Main.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.compilador.model;
+
+import com.mycompany.compilador.view.MainMenu;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ *
+ * @author gabriel.kresin
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException ex) {
+            System.out.println("Erro ao carregar o tema");
+        }
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.setVisible(true);
+    }
+    
+}
